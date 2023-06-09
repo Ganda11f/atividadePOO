cb6a4af14f8f225bd7ba50f38cca9e7c39ffd105
Lucas-SSG <113908890+Ganda11f@users.noreply.github.com>
2023-06-09 13:00:02 -0300

    Update 123d).php

--- 123d).php
+++ 123d).php
@@ -1,11 +1,22 @@
-<?php 
-class Conta{
+<?php
+class Conta {
     private $titulo;
     private $banco;
     private $saldo;
-    public function depositar (){
-        }    public function sacar(){
-            
+
+    public function depositar($valor) {
+        $this->saldo += $valor;
+    }
+
+    public function sacar($valor) {
+        if ($valor <= $this->saldo) {
+            $this->saldo -= $valor;
+        } else {
+            echo "Saldo insuficiente.";
         }
+    }
+
+    public function getSaldo() {
+        return $this->saldo;
+    }
 }
-?>
